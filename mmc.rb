1ab64dea471ab521e54ead8a955f3dff67127991
Simon Chatteleyn <kilik2049@gmail.com>
2012-10-19 17:22:40 +0200

    Change spark for statistical by noqqe. Have a stackLevelError to fix

--- mmc.rb
+++ mmc.rb
@@ -1,6 +1,5 @@
 =begin
 MetaMetaCritic is originally coded by jyli7. See the repo at https://github.com/jyli7/Meta-metacritic.
-I'm currently refactoring the code to match my need.
 =end
 
 #!/usr/bin/env ruby
@@ -14,136 +13,136 @@ APIKEY = "hv4pzbs4n46nmv7s9w87nzwu"
 
 def mmc(title)
 # Method for converting fractions to decimals. Used below in "calculate_average" method.
-def frac_to_float(str)
-    numerator, denominator = str.split("/").map(&:to_f)
-    denominator ||= 1
-    numerator/denominator
-end
+  def frac_to_float(str)
+      numerator, denominator = str.split("/").map(&:to_f)
+      denominator ||= 1
+      numerator/denominator
+  end
 
-def identify_movie(title) 
-  url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{APIKEY}&q=#{title}&page_limit=20"
-  buffer = open(url).read   
-  # convert JSON data into a hash
-  result = JSON.parse(buffer)
-  result['movies'].each do |movie|
-    if movie['title'] == title
-      return movie
+  def identify_movie(title) 
+    url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{APIKEY}&q=#{title}&page_limit=20"
+    buffer = open(url).read   
+    # convert JSON data into a hash
+    result = JSON.parse(buffer)
+    result['movies'].each do |movie|
+      if movie['title'] == title
+        return movie
+      end
     end
   end
-end
 
-def rt(id)
+  def rt(id)
 
-    def get_movie(id)
-        url = "http://api.rottentomatoes.com/api/public/v1.0/movies/#{id}.json?apikey=#{APIKEY}"
+      def get_movie(id)
+          url = "http://api.rottentomatoes.com/api/public/v1.0/movies/#{id}.json?apikey=#{APIKEY}"
+          buffer = open(url).read
+          # convert JSON data into a hash
+          result = JSON.parse(buffer)
+          return result
+      end
+
+      # Pull up reviewers list
+
+      def get_all_critics(id)
+        url = "http://api.rottentomatoes.com/api/public/v1.0/movies/#{id}/reviews.json?review_type=all&page_limit=30&page=1&country=us&apikey=#{APIKEY}"
         buffer = open(url).read
         # convert JSON data into a hash
         result = JSON.parse(buffer)
         return result
-    end
+      end 
 
-    # Pull up reviewers list
+      #Converts critic ratings, e.g. "A", "5/5", "78/100", to a 100 point scale
+      def score_convert(n) 
+          score = 0
+          if n.length <= 2 #if the score is in "A", "A+", "C-" form
+              case n[0] #to account for the first letter
+              when "A"
+              score = 95
+              when "B"
+              score = 85
+              when "C"
+              score = 75
+              when "D"
+              score = 65
+              else
+              score = 50
+              end
+          
+              case n[1] #to account for + and -
+              when "+"
+              score += 3
+              when "-"
+              score -=3
+              end
+          end 
+          if n.include? "/"  #if the score is in X/Y form
+              score = (frac_to_float(n)*100).to_i
+          end
+          score
+      end
 
-    def get_all_critics(id)
-      url = "http://api.rottentomatoes.com/api/public/v1.0/movies/#{id}/reviews.json?review_type=all&page_limit=30&page=1&country=us&apikey=#{APIKEY}"
+      def display_final_stats(movie_critics)
+          sum = 0
+          count = 0
+          movie_critics["reviews"].each_with_index do |a, index| 
+              if a["original_score"] 
+              converted_score = score_convert(a["original_score"])
+              sum += converted_score 
+              
+              count += 1
+              end
+          end
+          
+          #Calculates average converted score, for all RT critics
+          avg_converted_score = ((sum.to_f)/count)
+          return avg_converted_score
+      end 
+      
+      # RUN FUNCTIONS #
+      movie_found = get_movie(id) #movie_found is a hash that has the basic movie info
+      movie_critics = get_all_critics(id) 
+      show_movie_details_score = display_final_stats(movie_critics) 
+  end 
+
+  def imdb(title) #returns the movie that the user selected
+
+      url = "http://www.imdbapi.com/?i=&t=#{title}"
       buffer = open(url).read
+
       # convert JSON data into a hash
-      result = JSON.parse(buffer)
+      json = JSON.parse(buffer)
+      result = json["imdbRating"].to_f * 10
       return result
-    end 
 
-    #Converts critic ratings, e.g. "A", "5/5", "78/100", to a 100 point scale
-    def score_convert(n) 
-        score = 0
-        if n.length <= 2 #if the score is in "A", "A+", "C-" form
-            case n[0] #to account for the first letter
-            when "A"
-            score = 95
-            when "B"
-            score = 85
-            when "C"
-            score = 75
-            when "D"
-            score = 65
-            else
-            score = 50
-            end
-        
-            case n[1] #to account for + and -
-            when "+"
-            score += 3
-            when "-"
-            score -=3
-            end
-        end 
-        if n.include? "/"  #if the score is in X/Y form
-            score = (frac_to_float(n)*100).to_i
-        end
-        score
-    end
+  end
 
-    def display_final_stats(movie_critics)
-        sum = 0
-        count = 0
-        movie_critics["reviews"].each_with_index do |a, index| 
-            if a["original_score"] 
-            converted_score = score_convert(a["original_score"])
-            sum += converted_score 
-            
-            count += 1
-            end
-        end
-        
-        #Calculates average converted score, for all RT critics
-        avg_converted_score = ((sum.to_f)/count)
-        return avg_converted_score
-    end 
-    
-    # RUN FUNCTIONS #
-    movie_found = get_movie(id) #movie_found is a hash that has the basic movie info
-    movie_critics = get_all_critics(id) 
-    show_movie_details_score = display_final_stats(movie_critics) 
-end 
-
-def imdb(title) #returns the movie that the user selected
-
-    url = "http://www.imdbapi.com/?i=&t=#{title}"
-    buffer = open(url).read
-
-    # convert JSON data into a hash
-    json = JSON.parse(buffer)
-    result = json["imdbRating"].to_f * 10
-    return result
-
-end
-
-def metacritic(title)
-    #insert metacritic scraping here
-    movie = Nokogiri::HTML(open("http://www.metacritic.com/movie/#{title}"))
-    rating = movie.at_css(".score_value").text
-    return rating
-end 
-    
+  def metacritic(title)
+      #insert metacritic scraping here
+      movie = Nokogiri::HTML(open("http://www.metacritic.com/movie/#{title}"))
+      rating = movie.at_css(".score_value").text
+      return rating
+  end 
+      
 #temp = in_theaters  
 
 #Get the title
-title.chomp!.gsub!(' ', '+') # sub spaces for plus signs
+  title = title.chomp.gsub(' ', '+') # sub spaces for plus signs
 
 #Find the movie, based on the user's input
-movie = identify_movie(title)
+  movie = identify_movie(title)
 
 #Reformat the title, so IMDB and MC will recognize it
-title_for_imdb = movie[0]["title"].downcase.gsub(" ", "+")
-title_for_mc = movie[0]["title"].downcase.gsub(" ", "-")
+  title_for_imdb = movie[0]["title"].downcase.gsub(" ", "+")
+  title_for_mc = movie[0]["title"].downcase.gsub(" ", "-")
 
 #Use ID to find the movie in RT
-id = movie[0]["id"]
+  id = movie[0]["id"]
 
 #Run the 3 main functions for RT, IMDB, and MC
-rt_score = rt(id) 
-mc_score = metacritic(title_for_mc).to_i
-imdb_score = imdb(title_for_imdb).to_i 
+  rt_score = rt(id) 
+  mc_score = metacritic(title_for_mc).to_i
+  imdb_score = imdb(title_for_imdb).to_i 
 
-meta_meta_score = ("%.2f" % ((rt_score + mc_score + imdb_score)/3.0))
-return meta_meta_score
+  meta_meta_score = ("%.2f" % ((rt_score + mc_score + imdb_score)/3.0))
+  return meta_meta_score
 end
--- movie.rb
+++ movie.rb
@@ -6,54 +6,51 @@ require 'Net/http'
 require 'json'
 require 'mmc.rb'
 
-if ENV["PATH"].split(':').any? {|x| FileTest.executable? "#{x}/spark" }
-  
-  puts "Your research: "
-	name = gets.chomp.gsub(' ', '+').to_s.downcase
+puts "Your research: "
+name = gets.chomp.gsub(' ', '+').to_s.downcase
 
-	def getFilmography(name)
-	  query = "http://api.allocine.fr/rest/v3/search?partner=YW5kcm9pZC12M3M&filter=person&q=#{name}&format=json"
-	  resp = Net::HTTP.get_response(URI.parse(query))
-	  data = resp.body
-	  result = JSON.parse(data)
-	  result['feed']['person'].each { |person| @id = person['code'] }
-	  if @id == nil
-	    puts "Can't find this person"
-	  end 
-	
-	  filmo = []
-	
-	  query = "http://api.allocine.fr/rest/v3/filmography?partner=YW5kcm9pZC12M3M&profile=medium&code=#{@id}&filter=movie&format=json"
-	  resp = Net::HTTP.get_response(URI.parse(query))
-	  data = resp.body
-	  result = JSON.parse(data)
-	  result['person']['participation'].each do |participation|        
-      if participation && (m = participation['movie']) && (r = m['release']) && (rs = r['releaseState']) && [nil, 3011].include?(rs['code']) then 
-        next
-      else
-        title = participation['movie']['originalTitle']
-        filmo.push(title)
-      end
+def getFilmography(name)
+  query = "http://api.allocine.fr/rest/v3/search?partner=YW5kcm9pZC12M3M&filter=person&q=#{name}&format=json"
+  resp = Net::HTTP.get_response(URI.parse(query))
+  data = resp.body
+  result = JSON.parse(data)
+  result['feed']['person'].each { |person| @id = person['code'] }
+  if @id == nil
+    puts "Can't find this person"
+  end 
+
+  filmo = []
+
+  query = "http://api.allocine.fr/rest/v3/filmography?partner=YW5kcm9pZC12M3M&profile=medium&code=#{@id}&filter=movie&format=json"
+  resp = Net::HTTP.get_response(URI.parse(query))
+  data = resp.body
+  result = JSON.parse(data)
+  result['person']['participation'].each do |participation|        
+    if participation && (m = participation['movie']) && (r = m['release']) && (rs = r['releaseState']) && [nil, 3011].include?(rs['code']) then 
+      next
+    else
+      title = participation['movie']['originalTitle']
+      filmo.push(title)
     end
-    
-    filmo.reverse!
-    return filmo
   end
 
-	filmo = getFilmography(name)
-  marks = []
-	
-	filmo.each do |title|
-	  note = mmc(title)
-	  marks.push(note)
-	end
-	
-	def graph(marks)
-	  marks = marks.join(',')
-	  puts "`spark` #{marks}"
-	end
-  
-else
-  $stderr.puts "You must install spark"
-  Process.exit
+  filmo.reverse!
+  return filmo
+end
+
+filmo = getFilmography(name)
+marks = []
+
+filmo.each do |title|
+  note = mmc(title)
+  marks.push(note)
+end
+
+def graph(filmo, marks)
+  elements = Hash[filmo.zip(marks)]
+  result = ""
+  elements.each do |movie, mark|
+    all = all + "#{movie}:#{mark} "
+  end
+  puts "`./v-barplot` #{result}"
 end
